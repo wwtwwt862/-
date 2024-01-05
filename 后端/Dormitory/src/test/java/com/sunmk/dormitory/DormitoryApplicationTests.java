bacddc4aa7bd13b9b4f9bb364e51b66a08047bda
sunmk <2465941889@qq.com>
2024-01-05 09:34:57 +0800

    后端框架

---
+++ 后端/Dormitory/src/test/java/com/sunmk/dormitory/DormitoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunmk.dormitory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormitoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
